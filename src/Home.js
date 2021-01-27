90479eb7a5f0251cab6b1dfce26a42f2e1873976
janvivishwakk <not.yajat@gmail.com>
2021-01-27 18:51:09 +0530

    fin

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,8 @@
-import React, { useEffect, useState } from "react";
-const faunadb = require("faunadb");
-const q = faunadb.query;
+import React from "react";
 
-const client = new faunadb.Client({
-  secret: process.env.REACT_APP_FAUNADB_SECRET_KEY,
-});
-const testFauna = () => {
-  console.log(process.env);
-  const data = client.query(
-    q.Get(
-      q.Ref(
-        q.Collection(process.env.REACT_APP_FAUNADB_COLLECTION),
-        process.env.REACT_APP_FAUNADB_REF
-      )
-    )
-  );
-  const values = data.then(function (response) {
-    return response.data;
-  });
-  return values;
-};
+import main from "./main.png";
 
 const Home = () => {
-  const [temperature, settemperature] = useState("");
-  const [spo2, setspo2] = useState("");
-  const [pr, setpr] = useState("");
-  const [lastMod, setlastMod] = useState(null);
-  useEffect(() => {
-    testFauna().then((data) => {
-      console.log(data);
-      setpr(data.pr);
-      setspo2(data.spo2);
-      settemperature(data.temperature);
-      setlastMod(data.last_modified);
-    });
-  }, []);
-  const time = new Date();
-  time.setTime(lastMod);
-
   return (
     <div className="grid-container">
       <div className="header">
@@ -47,7 +12,10 @@ const Home = () => {
           2021. It has been a very tricky and long fight and I thank my parents
           who have been extremely supportive throughout. And to all my beautiful
           friend(s), Thank you for tagging along with me during this journey.
-          Your care and Support kept me sane.
+          Your care and support have kept me sane.
+        </div>
+        <div className="lastmod">
+          <img src={main} alt="byby" className="responsive-img" />
         </div>
       </div>
     </div>
