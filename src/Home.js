51a9482f4d0d1ed7032da868bb590ad96bb615e3
janvivishwakk <not.yajat@gmail.com>
2021-01-27 18:41:55 +0530

    fin

--- src/Home.js
+++ src/Home.js
@@ -41,60 +41,13 @@ const Home = () => {
   return (
     <div className="grid-container">
       <div className="header">
-        <div className="title">How is yajat doing?</div>
+        <div className="title">In the loving memory of COVID19...</div>
         <div className="lastmod">
-          This is a record system because i am bored and i have a lot of time
-          since i have COVID19. Screw you, China. My best wishes with those who
-          are presently suffering through corona. Speedy recovery guys!
-        </div>
-        <div className="sym">
-          Actual Symptoms
-          <ul>
-            <li>Very mild cough. Almost negligible</li>
-            <li>No temperature</li>
-            <li>Body at Normal functioning </li>
-            <li>
-              Medications:
-              <ul>
-                <li>Fabi Flu 800 anti viral</li>
-                <li>Cee plus - Multi Vitamins</li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-        <div className="lastmod">
-          Last updated:{" "}
-          <b>
-            {time.toDateString()} -{" "}
-            {time.toLocaleString("en-US", {
-              hour: "numeric",
-              minute: "2-digit",
-              hour12: true,
-            })}
-          </b>
-        </div>
-      </div>
-
-      <div className="main">
-        <div class="parent">
-          <div class="box">
-            <div onClick={testFauna} className="card">
-              <div className="value">{temperature} F</div>
-              <div className="text">Temperature</div>
-            </div>
-          </div>
-          <div class="box">
-            <div className="card">
-              <div className="value">{spo2} %</div>
-              <div className="text">SPO2 level</div>
-            </div>
-          </div>
-          <div className="box">
-            <div className="card">
-              <div className="value">{pr} bpm</div>
-              <div className="text">Pulse Rate</div>
-            </div>
-          </div>
+          I have officially tested negative for the COVID19 test on 27 January,
+          2021. It has been a very tricky and long fight and I thank my parents
+          who have been extremely supportive throughout. And to all my beautiful
+          friend(s), Thank you for tagging along with me during this journey.
+          Your care and Support kept me sane.
         </div>
       </div>
     </div>
